2341f0054310e3af8d4f7af1cc33294087e78106
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-07-25 19:51:49 -0700

    imported the data into the wrapper components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -3,8 +3,10 @@ import React from "react";
 //import any components needed
 
 //Import your array data to from the provided data file
+import numbers from "../../../data";
 
 const Numbers = () => {
+  console.log(numbers);
   // STEP 2 - add the imported data to state
   return (
     <div>
@@ -14,3 +16,5 @@ const Numbers = () => {
     </div>
   );
 };
+
+export default Numbers;
