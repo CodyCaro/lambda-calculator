3d8c273588e82225cc797c58e732a478becb9cd2
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-07-25 19:54:01 -0700

    Added the data to the state

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
@@ -6,8 +6,8 @@ import React from "react";
 import numbers from "../../../data";
 
 const Numbers = () => {
-  console.log(numbers);
   // STEP 2 - add the imported data to state
+  const [numbersState, setNumbersState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
